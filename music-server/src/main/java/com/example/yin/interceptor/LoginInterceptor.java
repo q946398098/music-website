da8709d0de11b3931eecee032571420a842cbba2
Martin.Hu <Martin.Hu@ttigroup.com>
2025-02-08 15:41:09 +0800

    全会了

--- music-server/src/main/java/com/example/yin/interceptor/LoginInterceptor.java
+++ music-server/src/main/java/com/example/yin/interceptor/LoginInterceptor.java
@@ -3,6 +3,8 @@ package com.example.yin.interceptor;
 
 import com.example.yin.common.R;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
@@ -17,11 +19,16 @@ import java.nio.charset.StandardCharsets;
 
 public class LoginInterceptor implements HandlerInterceptor {
 
+    private static final Logger logger = LoggerFactory.getLogger(LoginInterceptor.class);
+
+
     @Autowired
     private ObjectMapper objectMapper;
     @Override
     public boolean preHandle( HttpServletRequest request,  HttpServletResponse response,  Object handler) throws IOException {
+
         HttpSession session = request.getSession(false); //true 总是确保返回一个会话对象，如果没有会话则创建一个 , false 只返回当前已存在的会话，如果没有会话则返回null
+        logger.info( "session-----------" + session );
         if ((session == null || session.getAttribute("name") == null) && !request.getMethod().equals("OPTIONS")){
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             response.setContentType("application/json");
